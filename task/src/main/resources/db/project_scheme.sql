24239df4a62b651b8265acd0b3952559ff4a7204
acer <nikitos2986@gmail.com>
2019-07-01 16:05:18 +0300

    add data to db

--- task/src/main/resources/db/project_scheme.sql
+++ task/src/main/resources/db/project_scheme.sql
@@ -16,3 +16,12 @@ CREATE TABLE `account` (
   CONSTRAINT `users_account` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
+insert into users (name, sureName) values ('Nikita', 'Ershov');
+insert into users (name, sureName) values ('Nikita1', 'Ershov1');
+insert into users (name, sureName) values ('Nikita2', 'Ershov2');
+insert into users (name, sureName) values ('Nikita3', 'Ershov3');
+
+insert into account (account, userid) values (12345, 1);
+insert into account (account, userid) values (12346, 2);
+insert into account (account, userid) values (12347, 3);
+insert into account (account, userid) values (12348, 4);
